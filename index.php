9736eb0f6076f24fbf64d417228d12aa68388e4a
Thomas Andrei <thomasandrei1991@gmail.com>
2025-12-02 20:38:19 +0800

    Completed

--- index.php
+++ index.php
@@ -2,12 +2,27 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="index.css">
         <title>Document</title>
     </head>
     <body>
+        <div class="button-container">
+            <h2>Login Form</h2>
+            <div class="form-container">  
+                <form action="index.php" method="post">
+                    <label for="username">Username :</label>
+                    <input type="text" id="username" name="username">
 
+                    <label for="password">Password :</label>
+                    <input type="password" id="password" name="password">
+
+                    <button type="submit" id="login">Login</button>
+                    <button type="reset" id="cancel">Cancel</button>
+                </form>
+            </div>
+        </div>  
     </body>
 </html>
 <?php
-    
+    include("main.php");
 ?>
